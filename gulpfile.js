65c7e7e462a4790cbe09b5eff586db1a8e2d973d
Kevin Ewing <kevin@Kevins-MacBook-Pro.local>
2015-10-09 23:20:50 -0700

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+
+gulp.task('concat-polyfills', function() {
+});
+
+gulp.task('concat-functions', function() {
+});
+
+gulp.task('build-node', function() {
+});
+
+gulp.task('build-client', function() {
+});
+
+gulp.task('build', function() {
+});
+
+gulp.task('default', function() {
+  console.log('init');
+});
